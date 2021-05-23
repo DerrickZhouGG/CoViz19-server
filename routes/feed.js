62a500f72268581d6c44f1881dd0f01b202de515
Ryan <ryan_yrs@hotmail.com>
2021-05-22 23:41:21 -0400

    Update

--- routes/feed.js
+++ routes/feed.js
@@ -6,24 +6,6 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-// GET /feed/posts
-router.get('/posts', isAuth, feedController.getPosts);
-
-// POST /feed/post
-router.post(
-  '/post',
-  isAuth,
-  [
-    body('title')
-      .trim()
-      .isLength({ min: 5 }),
-    body('content')
-      .trim()
-      .isLength({ min: 5 })
-  ],
-  feedController.createPost
-);
-
 router.get('/post/:postId', isAuth, feedController.getPost);
 
 router.put(
